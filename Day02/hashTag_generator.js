10e7d8a4bb2d5692bae425961b8060b09537553b
Samee <samiuzzama619@gmail.com>
2024-04-02 14:58:11 +0530

    hash tag generator function

---
+++ Day02/hashTag_generator.js
@@ -0,0 +1,38 @@
+//*-
+//* Programming Question: Hash Tag Generator
+//*.
+
+//? You are required to implement a function generateHash that generates a hash tag from a given input string.
+
+// The hash tag should be constructed as follows:
+//? The input string should be converted to a hash tag format, where each word is capitalized and concatenated
+// together without spaces.
+
+//? If the length of the input string is greater than 280 characters or if the input string is empty or
+// contains only whitespace, the function should return false.
+
+//? Otherwise, the function should return the generated hash tag prefixed with #.
+
+//* Write a function generateHash to accomplish this task.
+
+const generateHash = (str) => {
+  if (str?.lenght > 280 || str?.trim() === 0) {
+    false;
+  }
+  let text = str?.split(" ");
+  //1St way
+  //   text = text?.map((e) => {
+  //     return e?.replace(e[0], e[0]?.toUpperCase());
+  //   });
+  //   text = `#${text?.join("")}`;
+  //   return text;
+
+  // 2nd way
+  text = text?.map((e) => {
+    return e?.charAt(0)?.toUpperCase() + e?.slice(1);
+  });
+  text = `#${text?.join("")}`;
+  return text;
+};
+console.log(generateHash("my name is samee uzzama"));
+// o/p "#MyNameIsSameeUzzama"
